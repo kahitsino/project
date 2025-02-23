3784bef96cf2e43165ce6451016b92e09aff4ba7
kahitsino <tolentinoaaron200@gmail.com>
2025-02-23 20:57:56 +0800

    initial commit

---
+++ project/js/script.js
@@ -0,0 +1,55 @@
+// Modal img 1
+var modal = document.getElementById("id-modal-gwyne1");
+
+var img = document.getElementById("modal-gwyne1");
+var modalImg = document.getElementById("img-id-modal-gwyne1");
+img.onclick = function() {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+}
+var span = document.getElementsByClassName("close1")[0];
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// Modal img 2
+var modal = document.getElementById("id-modal-gwyne2");
+
+var img = document.getElementById("modal-gwyne2");
+var modalImg = document.getElementById("img-id-modal-gwyne2");
+img.onclick = function() {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+}
+var span = document.getElementsByClassName("close2")[0];
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// Modal img 3
+var modal = document.getElementById("id-modal-gwyne3");
+
+var img = document.getElementById("modal-gwyne3");
+var modalImg = document.getElementById("img-id-modal-gwyne3");
+img.onclick = function() {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+}
+var span = document.getElementsByClassName("close3")[0];
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// Modal img 4
+var modal = document.getElementById("id-modal-gwyne4");
+
+var img = document.getElementById("modal-gwyne4");
+var modalImg = document.getElementById("img-id-modal-gwyne4");
+img.onclick = function() {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+}
+var span = document.getElementsByClassName("close4")[0];
+span.onclick = function() {
+    modal.style.display = "none";
+}
