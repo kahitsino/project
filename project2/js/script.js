6b4398828e33717b2a8fbab87f205aadea27c61e
kahitsino <tolentinoaaron200@gmail.com>
2025-02-23 21:27:34 +0800

    initial commit

---
+++ project2/js/script.js
@@ -0,0 +1,83 @@
+// Modal img 1
+var modal = document.getElementById("id-modal-lyn1");
+
+var img = document.getElementById("modal-lyn1");
+var modalImg = document.getElementById("img-id-modal-lyn1");
+img.onclick = function() {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+}
+var span = document.getElementsByClassName("close1")[0];
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// Modal img 2
+var modal = document.getElementById("id-modal-lyn2");
+
+var img = document.getElementById("modal-lyn2");
+var modalImg = document.getElementById("img-id-modal-lyn2");
+img.onclick = function() {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+}
+var span = document.getElementsByClassName("close2")[0];
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// Modal img 3
+var modal = document.getElementById("id-modal-lyn3");
+
+var img = document.getElementById("modal-lyn3");
+var modalImg = document.getElementById("img-id-modal-lyn3");
+img.onclick = function() {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+}
+var span = document.getElementsByClassName("close3")[0];
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// Modal img 4
+var modal = document.getElementById("id-modal-lyn4");
+
+var img = document.getElementById("modal-lyn4");
+var modalImg = document.getElementById("img-id-modal-lyn4");
+img.onclick = function() {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+}
+var span = document.getElementsByClassName("close4")[0];
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// Modal img 5
+var modal = document.getElementById("id-modal-lyn5");
+
+var img = document.getElementById("modal-lyn5");
+var modalImg = document.getElementById("img-id-modal-lyn5");
+img.onclick = function() {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+}
+var span = document.getElementsByClassName("close5")[0];
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// Modal img 6
+var modal = document.getElementById("id-modal-lyn6");
+
+var img = document.getElementById("modal-lyn6");
+var modalImg = document.getElementById("img-id-modal-lyn6");
+img.onclick = function() {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+}
+var span = document.getElementsByClassName("close6")[0];
+span.onclick = function() {
+    modal.style.display = "none";
+}
